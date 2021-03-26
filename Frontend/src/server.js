229d88ed1abd4fb8921048b0242adbc0a4287a18
Nour Yehia <noureldine.yehia@gmail.com>
2021-03-26 14:01:38 -0700

    fixed indenting

--- Frontend/src/server.js
+++ Frontend/src/server.js
@@ -6,15 +6,15 @@
 import api from "./config"
 
 const server = {
-  /* Method name: getName
-    * Author: @nouryehia
-    * Description: Gets name of user with given id
-    * Param: user_id:  id of user
-    * Returns:
-    *  { "reason": ..., "result": < name of user or null (depending on whether
-    *    user exists) >}
-    */
-  getName(user_id) {
+	/* Method name: getName
+	 * Author: @nouryehia
+     * Description: Gets name of user with given id
+     * Param: user_id:  id of user
+     * Returns:
+     *  { "reason": ..., "result": < name of user or null (depending on whether
+     *    user exists) >}
+     */
+	getName(user_id) {
 		const params = {user_id: user_id}
 		return api.get("user/get_name", {params: params})
 	}
